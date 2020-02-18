2c20a915dc708cb668b4df57196a63b247d54846
ruk <ruk.lakhani@students.makeschool.com>
2020-02-18 14:45:07 -0800

    heroku moree!

--- tweetGenerator/app.py
+++ tweetGenerator/app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 from dictogram import *
-
+import os 
 
 word_list = open_file("text.txt")
 frank_hist = Dictogram(word_list)
+
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/tweetGen')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
+tweets = db.tweets
 
 app = Flask(__name__)
 
@@ -17,3 +22,6 @@ def random_words():
     for _ in range(5):
         rand_words.append(frank_hist.sample())
     return render_template('random_words.html', word_list=rand_words)
+
+if __name__ == '__main__': 
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
